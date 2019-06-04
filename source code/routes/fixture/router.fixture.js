fd018fa2f9bf645365a4291a9d5b5292f73306fa
nghianino <nghiatdh98@gmail.com>
2019-06-04 10:34:09 +0700

    Thêm UI Ghi nhận kết quả

--- source code/routes/fixture/router.fixture.js
+++ source code/routes/fixture/router.fixture.js
@@ -24,6 +24,7 @@ router.get('/add-fixture', (req,res) => {
         activeLichthidau: true,
         cssfiles: [
             '../../public/vendors/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker-standalone.css',
         ],
         jsfiles: [
             '../../public/vendors/datatables.net/js/jquery.dataTables.min.js',
